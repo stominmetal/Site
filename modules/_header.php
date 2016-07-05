1e23a2f68058f6d05178c9d92d65c62c3d100320
stominmetal <stoyan@astrapaging.com>
2016-07-05 15:26:46 +0300

    Added-Navigation

--- modules/_header.php
+++ modules/_header.php
@@ -1,16 +1,26 @@
-<header class="row">
-    <a class="col" href="#">
-        <img src="images/logo.png" alt="Vessel Tracking">
-    </a>
+<header>
+    <div class="row">
+        <a class="col" href="#">
+            <img src="images/logo.png" alt="Vessel Tracking">
+        </a>
 
-    <div class="col" >
-        <h1>Vessel Tracking</h1>
-        <h2>AIS, satellite, ship tracking technology</h2>
+        <div class="col">
+            <h1>Vessel Tracking</h1>
+            <h2>AIS, satellite, ship tracking technology</h2>
+        </div>
+
+        <div class="col col-r search">
+            <input type="text" class="textSearch" name="searchbox">
+            <input type="button" class="btn" name="submit" value="Search">
+        </div>
     </div>
 
-    <div class="col col-r search">
-        <input type="text" name="searchbox">
-        <input type="button" name="submit" value="Search">
-    </div>
+    <nav class="row">
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Cruise Ships</a></li>
+            <li><a href="#">Container Ships</a></li>
+        </ul>
+    </nav>
 
 </header>
