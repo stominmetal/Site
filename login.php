5f5e6a445a1709100c85a4c69fed86964f02875f
stominmetal <stoyan@astrapaging.com>
2016-07-07 11:39:05 +0300

    Input-Placeholder

--- login.php
+++ login.php
@@ -16,9 +16,9 @@
     <div class="col col-m"></div>
     <div class="col col-m login">
         <h1 class="labels">Username</h1>
-        <input type="text" alt="login" name="login">
+        <input type="text" alt="username" name="username" placeholder="Enter username">
         <h1 class="labels">Password</h1>
-        <input type="password" alt="register" name="register" >
+        <input type="password" alt="password" name="pass" placeholder="Enter password">
         <h1 class="labels"></h1>
         <input type="button" value="Login" alt="buttonLogin" name="btnLogin">
         <p class="par">If you are not registered click <a href="#">here</a> to register.</p>
