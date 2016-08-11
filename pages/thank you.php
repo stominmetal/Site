be6e0c84a72987e103ea47134e70ef863fd62abb
stominmetal <stoyan@astrapaging.com>
2016-08-11 12:48:58 +0300

    Photos and Thumbs

---
+++ pages/thank you.php
@@ -0,0 +1,10 @@
+<div class="row">
+    <div class="border register-container">
+        <div class='border authentication'>
+            <h1>Thank you for uploading</h1>
+            <input type="submit" class="btn redBtn" onclick="window.location.replace('?page=home')" id="home" value="Home">
+            <input type="submit" class="btn redBtn" onclick="window.location.replace('?page=upload')" id="upload" value="Upload another picture">
+            <input type="submit" class="btn redBtn" onclick="window.location.replace('?page=upload')" id="upload" value="View All Photos">
+        </div>
+    </div>
+</div>
